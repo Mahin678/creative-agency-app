c89bd437cc03574ec5a133740df59c7a0bc046ac
Mahin678 <mahintazuar678@gmail.com>
2020-11-29 03:43:37 +0600

    update

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,47 +1,56 @@
 import React from 'react';
-import './footer.css'
+import './footer.css';
 const Footer = () => {
-    return (
-        <footer className="footer" >
-            <div className="footer-wrapper p-5">
-                <div className="row">
-                    <div className="col-sm-12 col-md-12 col-lg-6">
-                        <div className="footer-content container p-5" >
-                            <h2 >
-                                Let us handle your project
-                                , professionally.
-                            </h2>
-                            <p>
-                                With well written codes, we build amazing apps for all platforms, mobile and web apps in general.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-sm-12 col-md-12 col-lg-6">
-                        <div className="footer-form pt-5">
-                            <form>
-                                <div className="form-group ">
-                                    <input type="email" className="form-control p-4" placeholder="write your Email address" />
+	return (
+		<footer className="footer">
+			<div className="footer-wrapper p-5" id="contact">
+				<div className="row">
+					<div className="col-sm-12 col-md-12 col-lg-6">
+						<div className="footer-content container p-5">
+							<h2>Let us handle your project , professionally.</h2>
+							<p>
+								With well written codes, we build amazing apps for all
+								platforms, mobile and web apps in general.
+							</p>
+						</div>
+					</div>
+					<div className="col-sm-12 col-md-12 col-lg-6">
+						<div className="footer-form pt-5">
+							<form>
+								<div className="form-group ">
+									<input
+										type="email"
+										className="form-control p-4"
+										placeholder="write your Email address"
+									/>
+								</div>
+								<div className="form-group">
+									<input
+										type="text"
+										className="form-control p-4"
+										placeholder="write  Your Company / your name "
+									/>
+								</div>
+								<div className="form-group">
+									<textarea
+										className="form-control p-4"
+										placeholder="Your message"
+									></textarea>
+								</div>
 
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" className="form-control p-4" placeholder="write  Your Company / your name " />
-                                </div>
-                                <div className="form-group">
-                                    <textarea className="form-control p-4" placeholder="Your message" ></textarea>
-                                </div>
-
-                                <button type="submit" className="btn btn-custom">Send</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <h6 className="m-auto text-center m-5 p-5"  >
-                copy  &copy; right orange labs 2020
-                </h6>
-        </footer>
-    );
+								<button type="submit" className="btn btn-custom">
+									Send
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<h6 className="m-auto text-center m-5 p-5">
+				copy &copy; right orange labs 2020
+			</h6>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/Component/Home/Topbar/Topbar.js
+++ src/Component/Home/Topbar/Topbar.js
@@ -1,50 +1,91 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Topbar.css'
+import './Topbar.css';
 const Topbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-            <img className="ml-5" src="https://i.imgur.com/SsnK22Z.png" alt="topbar-logo" />
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <NavLink activeStyle={{
-                            fontWeight: "bold",
-                            color: "red"
-                        }} activeClassName="nav-style" className="nav-link a mr-3" to='/home'>Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink activeStyle={{
-                            fontWeight: "bold",
-                            color: "red"
-                        }} className="nav-link a mr-3" to='/home'>Our Portfolio</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink activeStyle={{
-                            fontWeight: "bold",
-                            color: "red"
-                        }} className="nav-link a mr-3" to='/home'>Our Team</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink activeStyle={{
-                            fontWeight: "bold",
-                            color: "red"
-                        }} className="nav-link a mr-3" to='/home'>Contact us</NavLink>
-                    </li>
-                    <NavLink to="/login">
-                        <button activeStyle={{
-                            fontWeight: "bold",
-                            color: "red"
-                        }} className="btn-custom">login</button>
-                    </NavLink>
-                </ul>
-            </div>
-        </nav>
-
-    );
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+			<img
+				className="ml-5"
+				src="https://i.imgur.com/SsnK22Z.png"
+				alt="topbar-logo"
+			/>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul className="navbar-nav ml-auto">
+					<li className="nav-item">
+						<NavLink
+							activeStyle={{
+								fontWeight: 'bold',
+								color: 'red',
+							}}
+							activeClassName="nav-style"
+							className="nav-link a mr-3"
+							to="/home"
+						>
+							Home
+						</NavLink>
+					</li>
+					<li className="nav-item">
+						<a
+							activeStyle={{
+								fontWeight: 'bold',
+								color: 'red',
+							}}
+							className="nav-link a mr-3"
+							href="#work"
+						>
+							Our Portfolio
+						</a>
+					</li>
+					<li className="nav-item">
+						<NavLink
+							activeStyle={{
+								fontWeight: 'bold',
+								color: 'red',
+							}}
+							className="nav-link a mr-3"
+							to="/home"
+						>
+							Our Team
+						</NavLink>
+					</li>
+					<li className="nav-item">
+						<a
+							activeStyle={{
+								fontWeight: 'bold',
+								color: 'red',
+							}}
+							className="nav-link a mr-3"
+							href="#contact"
+						>
+							Contact us
+						</a>
+					</li>
+					<NavLink to="/login">
+						<button
+							activeStyle={{
+								fontWeight: 'bold',
+								color: 'red',
+							}}
+							className="btn-custom"
+						>
+							login
+						</button>
+					</NavLink>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Topbar;
